846d3f6e92dc6197da3dcadbb1d5b6db654a99f3
dottokuya <55634529+dottokuya@users.noreply.github.com>
2020-01-05 18:11:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mandex/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.mandex;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
+    private ProgressBar progress;
+
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((int) R.layout.activity_main);
+        this.mywebView = (WebView) findViewById(R.id.webview);
+        this.progress = (ProgressBar) findViewById(R.id.progressBar);
+        this.mywebView.getSettings().setJavaScriptEnabled(true);
+        this.mywebView.loadUrl("https://mangadex.cc/");
+        this.mywebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                String main_url = "https://mangadex.cc";
+                if (url != null && url.startsWith(main_url)) {
+                    return false;
+                }
+                view.getContext().startActivity(new Intent("android.intent.action.VIEW", Uri.parse(url)));
+                return true;
+            }
+
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                if (progress != null)
+                    MainActivity.this.progress.setVisibility(View.VISIBLE);
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                if (progress != null)
+                    MainActivity.this.progress.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public void onBackPressed() {
+        if (this.mywebView.canGoBack()) {
+            this.mywebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mandex/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.mandex;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends Activity {
+    Handler handler;
+
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        this.handler = new Handler();
+        this.handler.postDelayed(new Runnable() {
+            public void run() {
+                SplashActivity.this.startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
