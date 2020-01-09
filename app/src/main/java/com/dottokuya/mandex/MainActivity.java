84f0a2627091684b05137375074596005e635202
dottokuya <55634529+dottokuya@users.noreply.github.com>
2020-01-09 13:56:42 +0700

    Add custom error page and swipe to reload function

--- app/src/main/java/com/dottokuya/mandex/MainActivity.java
+++ app/src/main/java/com/dottokuya/mandex/MainActivity.java
@@ -4,24 +4,39 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mywebView;
+    private SwipeRefreshLayout swipe;
     private ProgressBar progress;
 
     /* access modifiers changed from: protected */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView((int) R.layout.activity_main);
+        swipe = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                mywebView.reload();
+            }
+        });
+        LoadWeb();
+    }
+    public void LoadWeb(){
         this.mywebView = (WebView) findViewById(R.id.webview);
         this.progress = (ProgressBar) findViewById(R.id.progressBar);
         this.mywebView.getSettings().setJavaScriptEnabled(true);
         this.mywebView.loadUrl("https://mangadex.cc/");
+        swipe.setRefreshing(true);
         this.mywebView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 String main_url = "https://mangadex.cc";
@@ -41,7 +56,20 @@ public class MainActivity extends AppCompatActivity {
             public void onPageFinished(WebView view, String url) {
                 if (progress != null)
                     MainActivity.this.progress.setVisibility(View.GONE);
+                swipe.setRefreshing(false);
             }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+                Log.e("error","Received Error on WebView. ERROR CODE is " + errorCode);
+                Log.e("error","description; " + description);
+                Log.e("error","failingUrl is " + failingUrl);
+                try{
+                    view.loadUrl("file:///android_asset/error.html?errorCode=" + errorCode + "&errorDescription=" + description);
+                } catch  (Exception e) {
+                    Log.e("error", e.toString());
+                }
+            }
+
         });
     }
 
